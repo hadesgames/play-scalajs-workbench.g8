dcc4752bb23c34f04c6c668eb939e3cd01ee86ac
Alexandru Tache <hadesgames@gmail.com>
2014-04-09 18:28:09 +0300

    Cleaned up the files

--- scalaJS/src/main/scala/example/ScalaJSExample.scala
+++ scalaJS/src/main/scala/example/ScalaJSExample.scala
@@ -1,17 +1,15 @@
-package example
-
 import scala.scalajs.js
 import js.Dynamic.{ global => g }
+import shared.SharedMessages
 import js.annotation.JSExport
 import scala.scalajs.js.annotation.JSExport
 import scala.scalajs.js.annotation.JSExport
-import shared.SharedMessages
 
 @JSExport
 object ScalaJSExample {
   @JSExport
   def main(): Unit = {
-    g.document.getElementById("scala-demo").textContent = "Helloworld !" + SharedMessages.itWorks
+    g.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
   }
 
   /** Computes the square of an integer.
